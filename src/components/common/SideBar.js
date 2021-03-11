10fe604f29437cd42f3d43ed2adc794e86d45123
charlie_sz <karol.szczepaniec@gopos.pl>
2021-03-11 20:35:13 +0100

    up

--- src/components/common/SideBar.js
+++ src/components/common/SideBar.js
@@ -4,7 +4,7 @@ import {Nav, NavbarBrand} from "react-bootstrap";
 
 export default function SideBar(props){
     return(
-        <Navbar bg={'dark'}>
+        <Navbar bg={'dark'} className={'w-25 h-auto'}>
             {/*<Navbar.Brand href={'#home'}>*/}
             {/*    Task Manger App*/}
             {/*</Navbar.Brand>*/}
