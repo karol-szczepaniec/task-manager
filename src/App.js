c28980960697322dc66a0d7cdea0e510e94b0497
charlie_sz <karol.szczepaniec@gopos.pl>
2021-03-16 17:05:47 +0100

    fix sidebar

--- src/App.js
+++ src/App.js
@@ -6,22 +6,29 @@ import UserList from "./components/users/UserList";
 import TaskBoard from "./components/tasks/TaskBoard";
 import UserProvider from "./components/common/UserContext";
 import {BrowserRouter as Router, NavLink, Route, Switch} from "react-router-dom";
+import {Container, Row, Col} from "react-bootstrap";
 
 export default function App() {
   return (
-    <div className="App">
+    <Container className={"h-100"} fluid>
         <UserProvider>
         <Router>
-            <SideBar/>
-            <Switch>
-                <Route path="/about" component={About}/>
-                <Route path="/user-list" component={UserList}/>
-                <Route path="/task-board" component={TaskBoard}/>
-                <Route path="/dashboard" component={Dashboard}/>
-                <Route path="*" component={Dashboard}/>
-            </Switch>
+                <Row className={'h-100'}>
+                     <Col style={{backgroundColor:"#343a40"}} md={2} className={'p-0'}>
+                        <SideBar/>
+                    </Col>
+                    <Col className={'p-0'}>
+                        <Switch>
+                            <Route path="/about" component={About}/>
+                            <Route path="/user-list" component={UserList}/>
+                            <Route path="/task-board" component={TaskBoard}/>
+                            <Route path="/dashboard" component={Dashboard}/>
+                            <Route path="*" component={Dashboard}/>
+                        </Switch>
+                    </Col>
+                </Row>
         </Router>
         </UserProvider>
-    </div>
+    </Container>
   );
 }
--- src/components/common/SideBar.js
+++ src/components/common/SideBar.js
@@ -5,40 +5,40 @@ import {HouseFill, InfoCircleFill, KanbanFill, PeopleFill} from "react-bootstrap
 
 export default function SideBar(props){
     return(
-        <Navbar bg={'dark'} className={'w-25 mh-100 flex-column p-0 m-0'}>
-            <Navbar.Brand href={'/dashboard'} className={'w-100 text-center mb-5 '}>
-                <h1 className={'text-primary'}>
+        <Navbar bg={'dark'} className={'flex-column p-0 pt-4'} sticky={"top"}>
+            <Navbar.Brand href={'/dashboard'} className={'text-center mb-5 '}>
+                <h2 className={'text-primary'}>
                     Task Todo
-                </h1>
+                </h2>
             </Navbar.Brand>
                 {/*{props.children}*/}
             <NavLink
                 exacly={"true"}
                 to="/dashboard"
                 activeClassName="is-active"
-                className={'w-100 text-left mt-3 p-2 pl-4'}>
-                <h4><HouseFill className={'mr-3'}/>HOME</h4>
+                className={'w-100 text-left mt-3 p-2 pl-3'}>
+                <h5><HouseFill className={'mr-2'}/>HOME</h5>
             </NavLink>
 
             <NavLink
                 to="/user-list"
                 activeClassName="is-active"
-                className={'w-100 text-left mt-3 p-2 pl-4'}>
-                <h4><PeopleFill className={'mr-3'}/>USER LIST</h4>
+                className={'w-100 text-left mt-3 p-2 pl-3'}>
+                <h5><PeopleFill className={'mr-2'}/>USER LIST</h5>
             </NavLink>
 
             <NavLink
                 to="/task-board"
                 activeClassName="is-active"
-                className={'w-100 text-left mt-3 p-2 pl-4'}>
-                <h4><KanbanFill className={'mr-3'}/>TASK BOARD</h4>
+                className={'w-100 text-left mt-3 p-2 pl-3'}>
+                <h5><KanbanFill className={'mr-2'}/>TASK BOARD</h5>
             </NavLink>
 
             <NavLink
                 to="/about"
                 activeClassName="is-active"
-                className={'w-100 text-left mt-3 p-2 pl-4'}>
-                <h4><InfoCircleFill className={'mr-3'}/>ABOUT</h4>
+                className={'w-100 text-left mt-3 p-2 pl-3'}>
+                <h5><InfoCircleFill className={'mr-2'}/>ABOUT</h5>
             </NavLink>
         </Navbar>
     )
